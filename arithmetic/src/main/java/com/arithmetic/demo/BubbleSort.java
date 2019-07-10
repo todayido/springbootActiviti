47091f9c5ff4c67dcbd014d7030435bc57dcdcdd
Eric Lee <2227800977@qq.com>
2019-07-10 11:14:42 +0800

    bullesort commit:

--- arithmetic/src/main/java/com/arithmetic/demo/BubbleSort.java
+++ arithmetic/src/main/java/com/arithmetic/demo/BubbleSort.java
@@ -7,24 +7,34 @@ public class BubbleSort {
     /**
      * @Description: 冒泡排序
      * 时间复杂度O(n²)【两个for循环】,空间复杂度O(1)【temp临时变量一个】
+     * 稳定的
+     * 最右边开始--，从
      */
     public static void main(String[] args) {
         int[] arr = {5, 3, 6, 1, 7, 4, 2};
 
-        for (int i = 0; i < arr.length-1; i++) {
-            int max = i;
-            for (int j = i+1; j < arr.length; j++) { //
-                if (arr[max] < arr[i]) {
-                    max = i;
+        for (int i = arr.length-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j,j+1);
                 }
             }
 
-            int temp = arr[i];
-            arr[i] = arr[max];
-            arr[max] = temp;
-
-            System.out.println("===========");
             System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    public static void swap(int[] arr, int j, int i) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+
+    public static void findMax(int []arr, int i){
+        for (int i1 = 0; i1 < i; i1++) {
+            if (arr[i1]>arr[i1+1]) {
+                swap(arr, i1, i1 + 1);
+            }
         }
     }
 }
